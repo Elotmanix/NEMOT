fixed tensor broadcasting

NMOT_playground.py
@@ -6,6 +6,10 @@ import torch.nn.functional as F
 from timeit import default_timer as timer
 import os
 from config import PreprocessMeta
+from data_fns import gen_data, QuadCost
+from models import Net_EOT, NE_mot_model
+from TaosBiEOT import dual_loss
+
 
 
 """
@@ -26,9 +30,18 @@ def main():
     os.environ["CUDA_VISIBLE_DEVICES"] = str(params['cuda_visible'])
     device = torch.device("cuda:0" if (torch.cuda.is_available() and params['device'] == 'gpu') else "cpu")
     print(f"Using {device}")
+    print(params)
 
     X = gen_data(params)
     X = X.to(device)
+
+    # d = params['dims'][0]
+    # n = params['n']
+    #
+    # X = torch.from_numpy(
+    #     np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float().to(device)  # uniform [-1/sqrt(d),1/sqrt(d)]^d
+    # Y = torch.from_numpy(np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float().to(device)
+    # X = torch.stack([X,Y], dim=-1)
 
     if params['alg'] == 'ne_mot':
         MOT_agent = MOT_NE_alg(params, device)
@@ -39,77 +52,11 @@ def main():
 
 
 ########
-def gen_params():
-    params = {
-        'batch_size': 128,
-        'epochs': 2000,
-        'lr': 1e-4,
-        'n': 50000,
-        'k': 2,
-        'eps': 0.5,
-        'cost': 'quad',  # options - quad, quad_gw, ip_gw
-        'alg': 'ne_mot',        #options - ne_mot, sinkhorn_mot,ne_gw, sinkhorn_gw
-        'hidden_dim': 32,
-        'mod': 'mot',       #options - mot, mgw
-        'seed': 42,
-        'data_dist': 'uniform',
-        # 'dims': [1,1,1,1,1,1,1,1],
-        # 'dims': [100,100,100,100,100,100,100,100],
-        'dims': [5,5,5,5,5,5],
-        'device': 'gpu',
-        'cuda_visible': 0
-    }
-    # TD: ADJUST DIMS TO K
-    params['batch_size'] = min(params['batch_size'],params['n'])
-    return params
 
 
-def gen_data(params):
-    if params['data_dist'] == 'uniform':
-        # generate k samples which are d-dimensional with n samples (from Taos's notebook)
-        X = []
-        for i in range(params['k']):
-            X.append(np.random.uniform(-1/np.sqrt(params['dims'][i]),1/np.sqrt(params['dims'][i]),(params['n'],params['dims'][i])).astype(np.float32))
-        X = torch.from_numpy(np.stack(X, axis=-1))
-
-        if params['alg'] not in ('ne_gw','ne_mot'):
-            MU = [(1 / params['n']) * np.ones(params['n'])]*params['k']
-            return X, MU
-
-        return X
 
 
-def QuadCost(data, mod='circle'):
-    k=data.shape[-1]
-    if mod == 'circle':
-        differences = [torch.norm(data[:, :, i] - data[:, :, (i + 1) % k], dim=1) for i in range(k)]
-    elif mod == 'tree':
-        # calculate loss according to tree structure
-        pass
-    else:
-        # calculate all pairwise quadratic losses
-        ###
-        # option 1 - through broadcasting:
-        # Expand 'data' to (n, d, k, k) by repeating it across new dimensions
-        data_expanded = data.unsqueeze(3).expand(-1, -1, -1, k)
-        data_t_expanded = data.unsqueeze(2).expand(-1, -1, k, -1)
 
-        # Compute differences using broadcasting (resulting shape will be (n, d, k, k))
-        differences = data_expanded - data_t_expanded
-
-        # Compute norms (resulting shape will be (n, k, k))
-        differences = torch.norm(differences, dim=1)
-        ###
-        # option 2 - via a nested loop (doesnt use tensor operations but performs half the computations)
-        # pairwise_norms = torch.zeros((n, k, k))
-        # for i in range(k):
-        #     for j in range(i + 1, k):
-        #         pairwise_norms[:, i, j] = torch.norm(data[:, :, i] - data[:, :, j], dim=1)
-        # differences += pairwise_norms.transpose(1, 2)
-        ###
-
-
-    return differences
 
 class MOT_NE_alg():
     def __init__(self, params, device):
@@ -119,31 +66,52 @@ class MOT_NE_alg():
         self.batch_size = params['batch_size']
         self.eps = params['eps']
         for i in range(params['k']):
-            model = NE_model(dim=params['dims'][i], hidden_dim=params['hidden_dim'])
-            model.to(device)
+            # model = NE_mot_model(dim=params['dims'][i], hidden_dim=params['hidden_dim'])
+            # model.to(device)
+            ### Tao's
+            d = params['dims'][i]
+            model = Net_EOT(dim=d, K=min(6*d,80), deeper=True).to(device)
+            ###
             self.models.append(model)
         self.cost = params['cost']
         self.opt = [torch.optim.Adam(list(self.models[i].parameters()), lr=params['lr']) for i in range(self.k)]
         self.device = device
+        self.cost_graph = params['cost_graph']
 
 
     def train_mot(self, X):
+        x_b = DataLoader(X, batch_size=self.batch_size, shuffle=True)
+        tot_loss = []
+        times = []
         for epoch in range(self.num_epochs):
-            x_b = DataLoader(X, batch_size=self.batch_size, shuffle=True)
-            l = 0
+            l = []
             t0 = timer()
             for i, data in enumerate(x_b):
                 self.zero_grad_models()
                 # data.to(self.device)
                 for k_ind in range(self.k):
+                    # k-margin loss
                     phi = [self.models[i](data[:,:,i]) for i in range(self.k)]  # each phi[i] should have shape (b,1)
                     e_term = self.calc_exp_term(phi, data)
-                    loss = -(sum(phi).mean() - self.eps*e_term + self.eps)
+                    loss = -(sum(phi).mean() - self.eps*e_term)
+                    ####
+                    # bimargin loss (for debugging)
+                    # cost = self.calc_cost(data)
+                    # loss = dual_loss(pred_x = phi[0], pred_y = phi[1], cost=cost, eps=self.eps)
+                    ####
                     loss.backward()
                     self.opt[k_ind].step()
-                l -= loss.item()
-            print(f'finished epoch {epoch}, loss={-loss:.5f}, took {timer()-t0:.2f} seconds')
+                    l.append(loss.item())
+            l = np.mean(l)
+            tot_loss.append(-l+ self.eps)
+            epoch_time = timer()-t0
+            times.append(epoch_time)
+
+            print(f'finished epoch {epoch}, loss={-l+ self.eps:.5f}, took {epoch_time:.2f} seconds')
             # print(f'finished epoch {epoch}, loss={l / i}')
+        tot_loss = np.mean(tot_loss[-10:])
+        avg_time = np.mean(times)
+        print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds')
         self.models_to_eval
 
     def calc_exp_term(self, phi, x):
@@ -152,20 +120,32 @@ class MOT_NE_alg():
         # calc loss tensor
         c = self.calc_cost(x)
 
-        # calc mapping:
-        reshaped_term = []
-        reshaped_c = []
-        for index, vec in enumerate(phi):
-            # Create a shape of length k with 1s except at the index position
-            shape = [1] * self.k
-            shape[index] = -1
-            reshaped_c.append(c[index].reshape(shape))
-            reshaped_term.append(vec.reshape(shape))
+        if self.cost_graph == 'circle':
+            # calc mapping:
+            reshaped_term = []
+            reshaped_c = []
+            for index, vec in enumerate(phi):
+                # Create a shape of length k with 1s except at the index position
+                shape = [1] * self.k
+                shape[index] = -1
+                reshaped_c.append(c[index].reshape(shape))
+                reshaped_term.append(vec.reshape(shape))
+            reshaped_term = sum(reshaped_term)
+            c = sum(reshaped_c)
+        elif self.cost_graph == 'full':
+            reshaped_term = []
+            for index, vec in enumerate(phi):
+                # Create a shape of length k with 1s except at the index position
+                shape = [1] * self.k
+                shape[index] = -1
+                # shape[-index] = -1
+                reshaped_term.append(vec.reshape(shape))
+            reshaped_term = sum(reshaped_term)
+            # reshaped_term = phi[0][None, :] + phi[1][:, None]
 
-        reshaped_term = sum(reshaped_term)
-        reshaped_c = sum(reshaped_c)
 
-        return torch.mean(torch.exp((reshaped_term-reshaped_c)/self.eps))
+
+        return torch.mean(torch.exp((reshaped_term-c)/self.eps))
 
 
     def calc_cost(self, data):
@@ -175,12 +155,13 @@ class MOT_NE_alg():
         :return:
         """
         if self.cost == 'quad':
-            cost = QuadCost(data, mod='circle')
+            cost = QuadCost(data, mod=self.cost_graph)
         elif self.cost == 'quad_gw':
-            cost = QuadCostGW(data, self.matrices)
+            # IMPLEMENT - cost = QuadCostGW(data, self.matrices)
+            pass
         elif self.cost == 'ip_gw':
-            cost = IPCostGW(data, self.matrices)
-
+            # IMPLEMENT - cost = IPCostGW(data, self.matrices)
+            pass
 
         # NOW - BROADCAST!!
         return cost
@@ -194,19 +175,6 @@ class MOT_NE_alg():
             model.eval()
 
 
-class NE_model(nn.Module):
-    def __init__(self, dim, hidden_dim=32):
-        super(NE_model, self).__init__()
-        self.dim = dim
-
-        # Taos's N-GW model
-        self.fc1 = nn.Linear(dim, self.dim)
-        self.fc2 = nn.Linear(self.dim, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 
 

TaosBiEOT.py
@@ -4,70 +4,135 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-
+from ot.bregman import sinkhorn_knopp
+import os
+from models import Net_EOT
+from data_fns import gen_data
 
 def main():
     # TD:
-    d = 8  # dimension
-    n_epoch = 100  # number of epoch each iteration
+    d = 5  # dimension
+    n_epoch = 200  # number of epoch each iteration
     eps = 0.5  # set regularization term (epsilon)
-    n = 2048  # sample size
-    K = 32  # neuron size
-    lr = 1e-3  # learning rate
+    n = 5000  # sample size
+    K = min(6*d,80)  # neuron size, tao had 32 fixed
+    lr = 5e-4  # learning rate - 1e-3 worked best for Taos semidual original
     bs = 64  # batch size
 
+    two_fns=True
+    solve_classic = False
+    fixed_vol = True
 
-    X = torch.from_numpy(
-        np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float()  # uniform [-1/sqrt(d),1/sqrt(d)]^d
-    Y = torch.from_numpy(np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float()
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device}")
+    print(f'd={d},n={n},eps={eps},lr={lr},bs={bs},two_fns={two_fns}, fixed_vol={fixed_vol}')
+
+    if fixed_vol:
+        X = torch.from_numpy(
+            np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float().to(device)  # uniform [-1/sqrt(d),1/sqrt(d)]^d
+        Y = torch.from_numpy(np.random.uniform(-1 / np.sqrt(d), 1 / np.sqrt(d), (n, d))).float().to(device)
+    else:
+        params = {
+            'k': 2,
+            'alg': 'ne_mot',
+            'dims': [d,d,d,d],
+            'data_dist': 'uniform',
+            'n': n
+        }
+        data_x = gen_data(params)
+        data_x = data_x.to(device)
+        X = data_x[:,:,0]
+        Y = data_x[:, :, 1]
+        # prev implementation:
+        # X = torch.from_numpy(
+        #     np.random.uniform(-0.5, 0.5, (n, d))).float().to(
+        #     device)  # uniform [-1/sqrt(d),1/sqrt(d)]^d
+        # Y = torch.from_numpy(np.random.uniform(-0.5, 0.5, (n, d))).float().to(device)
+
+    if solve_classic:
+        log,gamma = solve_unif_sinkhorn(X,Y,eps,n)
 
 
 
-    f = Net_EOT(d, K)
-    optim_f = torch.optim.Adam(list(f.parameters()), lr=lr)
+    if two_fns:
+        deeper=True
+        f1 = Net_EOT(d, K, deeper).to(device)
+        f2 = Net_EOT(d, K, deeper).to(device)
+        # optim_f = torch.optim.Adam(list(f1.parameters())+list(f2.parameters()), lr=lr)
+        optim_f1 = torch.optim.Adam(list(f1.parameters()), lr=lr)
+        optim_f2 = torch.optim.Adam(list(f2.parameters()), lr=lr)
+    else:
+        f = Net_EOT(d, K).to(device)
+        optim_f = torch.optim.Adam(list(f.parameters()), lr=lr)
 
-
+    x_b = DataLoader(X, batch_size=bs, shuffle=True)
+    y_b = DataLoader(Y, batch_size=bs, shuffle=True)
+    epoch_losses = []
     for epoch in range(n_epoch):
-        x_b = DataLoader(X, batch_size=bs, shuffle=True)
-        y_b = DataLoader(Y, batch_size=bs, shuffle=True)
+        epoch_loss = []
         for x, y in zip(x_b, y_b):
-            optim_f.zero_grad()
-            pred_x = f(x)
+            if two_fns:
+                for k in range(2):
+                    if k:
+                        optim_f1.zero_grad()
+                    else:
+                        optim_f2.zero_grad()
+                    pred_x = f1(x)
+                    pred_y = f2(y)
+                    cost = torch.norm(x[:, None] - y[None, :], dim=-1) ** 2
+                    loss = dual_loss(pred_x, pred_y, cost, eps)
+                    loss.backward()
+                    if k:
+                        optim_f1.step()
+                    else:
+                        optim_f2.step()
+                epoch_loss.append(-loss.item()+eps)
+            else:
+                optim_f.zero_grad()
+                pred_x = f(x)
+                cost = torch.norm(x[:,None] - y[None,:], dim=-1) ** 2
+                loss = semidual_loss(pred_x, cost, eps)
+                loss.backward()
+                optim_f.step()
+                epoch_loss.append(-loss.item())
+        epoch_losses.append(epoch_loss)
+        epoch_loss = np.mean(epoch_loss)
+        print(f'loss: {epoch_loss}')
+    print(f'final 10 epochs average to {np.mean(epoch_losses[-10:])}')
+    # f.eval()
 
-            x_norm = torch.norm(x, dim=-1) ** 2
-            y_norm = torch.norm(y, dim=-1) ** 2
-            cost_ = -4 * x_norm[:, None] * y_norm[None, :]
 
-            cost = torch.norm(x[:,None] - y[None,:], dim=-1) ** 2
 
-            loss = semidual_loss(pred_x, cost, eps)
-            loss.backward()
-            optim_f.step()
-        print(f'loss: {loss.item()}')
-    f.eval()
+def solve_unif_sinkhorn(X,Y,eps,n):
+    a = (1 / n) * np.ones(n)
+    b = (1 / n) * np.ones(n)
+
+    C = np.linalg.norm(X[:,None] - Y[None,:], axis=-1) ** 2
+    p, log = sinkhorn_knopp(a=a,b=b,M=C,reg=eps, verbose=True, log=True)
+    return p,log
 
 
 
 
 def semidual_loss(pred_x,cost,eps):
-    max_x = pred_x.max() # stablized
-    ret = torch.mean(pred_x) - eps* torch.mean(torch.log(torch.mean(torch.exp((pred_x-cost-max_x)/eps),dim=0))) - max_x + eps
+    # max_x = pred_x.max() # stablized
+    max_x = 0
+    ret = torch.mean(pred_x) - eps* torch.mean(torch.log(torch.mean(torch.exp((pred_x-cost-max_x)/eps),dim=0))) - max_x
+    # ret = torch.mean(pred_x) - eps* torch.mean(torch.log(torch.mean(torch.exp((pred_x-cost-max_x)/eps),dim=0))) +eps
+    loss = - ret  # maximize
+    return loss
+
+def dual_loss(pred_x, pred_y, cost,eps):
+    # max_x = pred_x.max() # stablized
+    max_x = 0
+    ret = torch.mean(pred_x) + torch.mean(pred_y) - eps*torch.mean(torch.exp((pred_x[None,:]+pred_y[:,None]-cost)/eps))
     # ret = torch.mean(pred_x) - eps* torch.mean(torch.log(torch.mean(torch.exp((pred_x-cost-max_x)/eps),dim=0))) +eps
     loss = - ret  # maximize
     return loss
 
 
-class Net_EOT(nn.Module):
-    def __init__(self,dim,K):
-        super(Net_EOT, self).__init__()
 
-        self.fc1 = nn.Linear(dim, K)
-        self.fc2 = nn.Linear(K, 1)
-
-    def forward(self, x):
-        x1 = F.relu(self.fc1(x))
-        x2 = self.fc2(x1)
-        return x2
 
 
 