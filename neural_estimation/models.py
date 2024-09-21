fixed pairwise plan calculation (circle NEMOT)

neural_estimation/models.py
@@ -52,12 +52,18 @@ class MOT_NE_alg():
                     phi = [self.models[i](data[:,:,i]) for i in range(self.k)]  # each phi[i] should have shape (b,1)
                     e_term = self.calc_exp_term(phi, data)
                     loss = -(sum(phi).mean() - self.eps*e_term)
+                    if self.params.regularize_pariwise_coupling and self.params.cost_graph != 'full':
+                        # print('regularize_pariwise_coupling')
+                        pairwise_reg = self.calc_pairwise_coupling_regularizer(phi, data)
+                        reg_loss = loss + self.params.regularize_pariwise_coupling_reg * pairwise_reg
+                        reg_loss.backward()
                     ####
                     # bimargin loss (for debugging)
                     # cost = self.calc_cost(data)
                     # loss = dual_loss(pred_x = phi[0], pred_y = phi[1], cost=cost, eps=self.eps)
                     ####
-                    loss.backward()
+                    else:
+                        loss.backward()
 
                     if self.params.clip_grads:
                         torch.nn.utils.clip_grad_norm_(self.models[k_ind].parameters(), self.params.max_grad_norm)
@@ -65,22 +71,33 @@ class MOT_NE_alg():
                     self.opt[k_ind].step()
                     l.append(loss.item())
 
+            print_plan_overall = False
+            if print_plan_overall:
+                phi_all = [self.models[i](X[:,:,i]) for i in range(self.k)]
+                print(self.calc_exp_term(phi_all, X))
+
             l = np.mean(l)
             self.tot_loss.append(-l+ self.eps)
             epoch_time = timer()-t0
             self.times.append(epoch_time)
             print(f'finished epoch {epoch}, loss={-l+ self.eps:.5f}, took {epoch_time:.2f} seconds')
 
+            print_debug = True
+            if epoch%10==0 and print_debug and self.params.cost_graph != 'full':
+                P = self.calc_plan(X)
+                ot_cost = self.calc_ot_cost(P,X)
+                print(f'ot_cost {ot_cost}')
+
             if self.params.schedule:
                 for sched in self.scheduler:
                     sched.step()
                     lr = sched.get_last_lr()[0]
-                    print(f'updated learning rate {lr}')
+                    # print(f'updated learning rate {lr}')
 
             # print(f'finished epoch {epoch}, loss={l / i}')
         self.models_to_eval
 
-    def save_results(self):
+    def save_results(self,X=None):
         tot_loss = np.mean(self.tot_loss[-10:])
         avg_time = np.mean(self.times)
         data_to_save = {
@@ -88,8 +105,15 @@ class MOT_NE_alg():
             'avg_time': avg_time,
             'tot_loss': self.tot_loss,
             'times': self.times,
-            'params': self.params
-            }
+            'params': self.params,
+            # 'plan': plan,
+        }
+        if self.params.cost_graph != 'full':
+            plan = self.calc_plan(X)
+            ###
+            ot_cost = self.calc_ot_cost(plan,X)
+            ###
+            data_to_save['ot_cost'] = ot_cost
         # Save path
         path = os.path.join(self.params.figDir, 'results.pkl')
 
@@ -98,11 +122,30 @@ class MOT_NE_alg():
             pickle.dump(data_to_save, file)
 
         if self.using_wandb:
-            wandb.log({'tot_loss': tot_loss, 'avg_time': avg_time})
+            wandb.log({'tot_loss': tot_loss,
+                       'avg_time': avg_time
+                       })
+            if self.params.cost_graph != 'full':
+                wandb.log(({
+                       'ot_cost': ot_cost
+                       }))
 
+        if self.params.cost_graph != 'full':
+            print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds, ot_cost {ot_cost:.5f}')
+        else:
+            print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds')
 
-        print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds')
-
+    def calc_ot_cost(self, P, X):
+        C = self.calc_cost(X)
+        # phi = [self.models[i](X[:, :, i]) for i in range(self.k)]
+        # e_term = torch.eye(phi[0].shape[0]).to(self.device)
+        # for i in range(self.k):
+        #     L = torch.exp((0.5 * (phi[i] + phi[(i + 1) % self.k].T) - C[i]) / self.eps)
+        #     e_term = (e_term @ L)
+        # normal = torch.trace(e_term)
+        ot_cost = sum([torch.sum(c * p) for (c, p) in zip(C, P)])
+        return ot_cost
+        # return ot_cost/normal
 
     def calc_exp_term(self, phi, x):
         # TD - IMPLEMENT MORE MEMORY EFFICIENT CALCULATION!!! DIVIDE LOSS!!
@@ -115,7 +158,7 @@ class MOT_NE_alg():
             e_term = torch.eye(n).to(self.device)
             for i in range(self.k):
                 L = torch.exp((0.5*(phi[i] + phi[ (i+1)%self.k ].T) - c[i])/self.eps)
-                e_term = (e_term @ L)*(1/self.batch_size)
+                e_term = (e_term @ L)*(1/n)
             return torch.trace(e_term)
             # # calc mapping:
             # reshaped_term = []
@@ -148,7 +191,10 @@ class MOT_NE_alg():
         :return:
         """
         if self.cost == 'quad':
-            cost = QuadCost(data, mod=self.cost_graph)
+            if self.cost_graph == 'circle' and self.params.euler == 1:
+                cost = QuadCost(data, mod='euler')
+            else:
+                cost = QuadCost(data, mod=self.cost_graph)
         elif self.cost == 'quad_gw':
             # IMPLEMENT - cost = QuadCostGW(data, self.matrices)
             pass
@@ -166,6 +212,195 @@ class MOT_NE_alg():
     def models_to_eval(self):
         for model in self.models:
             model.eval()
+
+    def calc_plan(self, X):
+        if self.cost_graph == 'circle':
+            phi = [self.models[i](X[:, :, i]) for i in range(self.k)]
+            c = self.calc_cost(X)
+            exp_terms = [torch.exp((0.5*(phi[i] + phi[ (i+1)%self.k ].T) - c[i])/self.eps) for i in range(self.k)]
+            ot_plan = []
+            for i in range(self.k):
+                P = self.calc_pairwise_plan(exp_terms,i, (i+1)%self.k )
+                if self.params.normalize_plan:
+                    P = P/torch.sum(P)
+                ot_plan.append(P)
+                if self.params.check_P_sum and self.params.using_wandb:
+                    print('h')
+                    wandb.log({f'ot_plan_{i}_sum': torch.sum(ot_plan[i]).item()})
+        else:
+            phi = [self.models[i](X[:, :, i]) for i in range(self.k)]
+            reshaped_term = []
+            for index, vec in enumerate(phi):
+                # Create a shape of length k with 1s except at the index position
+                shape = [1] * self.k
+                shape[index] = -1
+                # shape[-index] = -1
+                reshaped_term.append(vec.reshape(shape))
+            reshaped_term = sum(reshaped_term)
+            # reshaped_term = phi[0][None, :] + phi[1][:, None]
+            c = self.calc_exp_term(phi, X)
+            ot_plan = torch.exp((reshaped_term - c) / self.eps)
+        return ot_plan
+
+    def calc_pairwise_plan_old(self, L,i,j):
+        '''
+        calculate the pairwise plan from margin i to margin j
+        TD (from pic) - start from (i,i+1) and then (k,1) and then general (i,j)?
+        Pi_{i,j} = \prod_{l=i}^{j-1}L_{l,l+1} \odot (\prod_{l=1}^{i-1}L_{l,l+1}) @ (\prod_{l=j+1}^{k}L_{l,l+1}).T
+        currently implemented for same number of samples.
+        '''
+        k = len(L)
+        n = L[0].shape[0]
+
+        if i == 0:
+            A = torch.eye(L[0].shape[0]).to(self.device)
+            B = torch.eye(L[0].shape[0]).to(self.device)
+
+            for l in range(k):
+                if l<j:
+                    A = A@L[l]
+                else:
+                    B = B@L[l]
+            C = A*B
+            print(f'adjacent with zero, P adds up to {torch.sum(C)}')
+            return C/(n**k)
+        elif j == (i+1)%k:
+            if j == i+1:
+                # two adjacent idx, i !=0, j=i+1
+                A = torch.eye(n).to(self.device)
+                B = torch.eye(n).to(self.device)
+                for l in range(k):
+                    if l <= i:
+                        A = A @ L[l]
+                    else:
+                        B = B @ L[l]
+                C = (A.T * B.T)
+                print(f'adjacent, P adds up to {torch.sum(C)/(n**k)}')
+                return C/(n**k)
+            else:
+                # cycle case or
+                # two adjacent idx, i !=0, j=i+1
+                A = torch.eye(n).to(self.device)
+                B = torch.eye(n).to(self.device)
+                for l in range(k):
+                    if l <= i:
+                        A = A @ L[l]
+                    else:
+                        B = B @ L[l]
+                C = (A * B.T)
+                print(f'adjacent, P adds up to {torch.sum(C) / (L[0].shape[0] ** k)}')
+                return C/(n**k)
+        else:
+            # GENERAL CASE
+            return
+
+    def calc_pairwise_plan_(self, L,i,j,verbose=True):
+        ### NEW IMPLEMENTATION
+        '''
+        calculate the pairwise plan from margin i to margin j
+        TD (from pic) - start from (i,i+1) and then (k,1) and then general (i,j)?
+        Pi_{i,j} = \prod_{l=i}^{j-1}L_{l,l+1} \odot (\prod_{l=1}^{i-1}L_{l,l+1}) @ (\prod_{l=j+1}^{k}L_{l,l+1}).T
+        currently implemented for same number of samples.
+        '''
+        k = len(L)
+        n = L[0].shape[0]
+
+        if i == 0:
+            A = torch.eye(L[0].shape[0]).to(self.device)
+            B = torch.eye(L[0].shape[0]).to(self.device)
+
+            for l in range(k):
+                if l<j:
+                    A = A@L[l]
+                    # A = A@L[l]/n
+                else:
+                    B = B@L[l]
+                    # B = B @ L[l] / n
+            C = A.T*B.T
+            if verbose:
+                print(f'adjacent, P adds up to {torch.sum(C)}')
+            return C
+        elif j == (i+1)%k:
+            if j == i+1:
+                # two adjacent idx, i !=0, j=i+1
+                A = torch.eye(n).to(self.device)
+                B = torch.eye(n).to(self.device)
+                for l in range(k):
+                    if l <= i:
+                        A = A @ L[l]
+                        # A = A @ L[l]/n
+                        # A = A @ L[l]/(n*L[l].sum(0))
+                    else:
+                        B = B @ L[l]
+                        # B = B @ L[l]/n
+                        # B = B @ L[l]/(n*L[l].sum(0))
+                C = (A.T * B.T)
+                if verbose:
+                    print(f'adjacent, P adds up to {torch.sum(C)}')
+                return C
+            else:
+                # cycle case or
+                # two adjacent idx, i !=0, j=i+1
+                A = torch.eye(n).to(self.device)
+                B = torch.eye(n).to(self.device)
+                for l in range(k):
+                    if l <= i:
+                        A = A @ L[l]
+                        # A = A @ L[l]/n
+                    else:
+                        B = B @ L[l]
+                        # B = B @ L[l] / n
+                C = (A.T * B.T)
+                if verbose:
+                    print(f'adjacent, P adds up to {torch.sum(C)}')
+                return C
+        else:
+            # GENERAL CASE
+            return
+
+    def calc_pairwise_plan(self, L, i, verbose=False):
+        # Compute A: Product of matrices up to index i-1
+        if i > 0:
+            A = L[0]
+            for k in range(1, i):
+                A = A @ L[k]
+        else:
+            # Use identity matrix if no matrices before index i
+            size = L[i].shape[0]
+            A = torch.eye(size, dtype=L[i].dtype, device=L[i].device)
+
+        # Compute B: Product of matrices from index i+1 to end
+        if i + 1 < len(L):
+            B = L[i + 1]
+            for k in range(i + 2, len(L)):
+                B = B @ L[k]
+        else:
+            # Use identity matrix if no matrices after index i
+            size = L[i].shape[1]
+            B = torch.eye(size, dtype=L[i].dtype, device=L[i].device)
+
+        # Calculate A.T @ B.T
+        C = A.T @ B.T
+
+        # Element-wise multiplication with L[i]
+        output = C * L[i]
+
+        # if verbose:
+        #     print(output.sum())
+
+        return output/output.sum()
+
+    def calc_pairwise_coupling_regularizer(self, phi, x):
+        c = self.calc_cost(x)
+        exp_terms = [torch.exp((0.5*(phi[i] + phi[ (i+1)%self.k ].T) - c[i])/self.eps) for i in range(self.k)]
+        reg = 0
+        for i in range(self.k):
+            ot_plan = self.calc_pairwise_plan(exp_terms, i, (i + 1) % self.k, verbose=False)
+            reg += (torch.sum(ot_plan)-1.0).abs()
+        return reg
+
+
+
 
 class Net_EOT(nn.Module):
     def __init__(self,dim,K,deeper=False):

sinkhorn/sinkhorn_utils.py
@@ -16,6 +16,7 @@ class MOT_Sinkhorn():
         self.k = params.k
         self.mus = MU
         self.n = params.n
+        self.ns = [params.n]*self.k
         self.tol = 1e-10
         if params.cost_graph == 'circle':
             self.tol = 1e-5
@@ -39,6 +40,7 @@ class MOT_Sinkhorn():
                 # Ci = cost(x_i,x_{i+1}), therefore has a shape (n x n)
                 # for i in range(self.k):
                 C = QuadCost(data=X, mod='circle')
+                self.C = C
                 K = [np.exp(-c/self.eps) for c in C]
 
 
@@ -134,7 +136,7 @@ class MOT_Sinkhorn():
             if naive:
                 mi = self.marginalize_naive(scalei)
             else:
-                mi = self.marginalize(scalei)
+                mi = self.marginalize(self.eta, self.phi, scalei)
 
             ratio = self.mus[scalei] / mi
             self.phi[scalei] = self.phi[scalei] + np.log(ratio) / eta
@@ -149,8 +151,56 @@ class MOT_Sinkhorn():
         return
 
 
+    def marginalize(self, eta, p, margidx):
+        """
+        Given weights p = [p_1,\ldots,p_k], and regularization eta > 0,
+        Let K = \exp[-C].
+        Let d_i = \exp[\eta p_i] for all i \in [k].
+        Let P = (d_1 \otimes \dots \otimes d_k) \odot K.
+        Return m_{margidx}(P).
+        """
 
-    def marginalize(self, margidx):
+        n = self.n
+        reg_cost = self.kernel
+        reg_loop_cost = self.kernel[self.k-1]
+
+        """ Compute transition matrices 1 --> margidx, margidx --> k """
+        trans1 = np.eye(n)
+        transk = np.eye(n)
+
+        # trans1[i,j] is the mass of cost of transitioning from i at time 1 to j at time margidx
+        # include the potentials in [1,margidx)
+        for i in range(margidx):
+            currcolscaling = np.diag(np.exp(eta * p[i]))
+            trans1 = trans1 @ currcolscaling
+            trans1 = trans1 @ reg_cost[i]
+
+        # transk[i,j] is the mass of the cost of transitioning from i at time margidx to j at time K
+        # include the potentials in (margidx,k]
+        for i in range(margidx+1, self.k):
+            transk = transk @ reg_cost[i-1]
+            currcolscaling = np.diag(np.exp(eta * p[i]))
+            transk = transk @ currcolscaling
+
+        # print('trans1',trans1)
+        # print('transk',transk)
+
+        # transk1[i,j] is the mass of the cost of transitioning from i at time margidx to j at time 1 (going through time k and looping back to time 1)
+        # includes the potentials in (margidx,k]
+        transk1 = transk @ reg_loop_cost
+
+        # For each fixed l, compute trans1[:,l] \dot transk1[l,:].
+        # This marginalizes over time 1, but still does not compute the potentials at margidx.
+        notscaled = np.diag(transk1 @ trans1)
+
+
+        scaled = notscaled * np.exp(eta * p[margidx])
+        # comparison = self.marginalize_naive(eta, p, margidx)
+        # assert(np.all(np.isclose(scaled, comparison)))
+
+        return scaled
+
+    def marginalize_(self, margidx):
         """
         Given weights p = [p_1,\ldots,p_k], and regularization eta > 0,
         Let K = \exp[-C].
@@ -239,7 +289,7 @@ class MOT_Sinkhorn():
                 mi = self.marginalize_naive(i)
             else:
                 # mi = self.marginalize_circle(i)
-                mi = self.marginalize(i)
+                mi = self.marginalize(self.eta, self.phi, i)
             badratio = self.mus[i] / mi
             minbadratio = np.minimum(1, badratio)
             p[i] = p[i] + np.log(minbadratio) / eta
@@ -250,7 +300,7 @@ class MOT_Sinkhorn():
                 mi = self.marginalize_naive(i)
             else:
                 # mi = self.marginalize_circle(i)
-                mi = self.marginalize(i)
+                mi = self.marginalize(self.eta, self.phi, i)
             erri = self.mus[i] - mi
             # COMMENTED THIS ASSERT!
             # assert(np.all(erri >= -1e-10))
@@ -273,7 +323,7 @@ class MOT_Sinkhorn():
             if naive:
                 mi = self.marginalize_naive(i)
             else:
-                mi = self.marginalize(i)
+                mi = self.marginalize(self.eta, self.phi, i)
             erri = np.sum(np.abs(mi - self.mus[i]))
             if erri > worsterr:
                 worsti = i
@@ -287,14 +337,21 @@ class MOT_Sinkhorn():
         :return:
         """
         if self.params.cost_graph == 'circle':
-            return 0
+            # Unregularized OT cost for Neural MOT plan.
+            if training:
+                return 0
+            P = self.calc_plan(training=False)
+            cost = sum([np.sum(c*p) for (c, p) in zip(self.C, P)])
+            return cost
+        # FULL ENTROPIC OT COST
         P = self.calc_plan(training)
         KL = sum([calc_ent(mu) for mu in self.mus]) - calc_ent(P)
         return np.sum(P*self.cost) + self.eps*KL
 
     def calc_plan(self, training):
         if self.params.cost_graph == 'circle':
-            P = self.circle_plan
+            P = self.circle_plan()
+            return P
         else:
             P = self.marginalize_naive(0, outplan=True)
             if training:
@@ -316,14 +373,14 @@ class MOT_Sinkhorn():
 
     def circle_plan(self):
         otmaps = []
-        for i in range(0, self.k - 1):
+        for i in range(0, self.k):
             print(f'Extracting map {i}')
-            otmaps.append(self.get_pairwise_marginal(i, i + 1))
+            otmaps.append(self.get_pairwise_marginal(self.eta, self.phi, self.rankone, i, (i + 1)%self.k ))
             print('Map extracted')
         return otmaps
 
 
-    def get_pairwise_marginal(self, i1, i2):
+    def get_pairwise_marginal_(self, i1, i2):
         pcopy = copy.deepcopy(self.phi)
         marg2 = np.zeros((self.ns[i1],self.ns[i2]))
         # print(np.sum(m1))
@@ -332,7 +389,7 @@ class MOT_Sinkhorn():
         for j1 in range(self.ns[i1]):
             pcopy[i1] = np.ones(self.ns[i1]) * -math.inf
             pcopy[i1][j1] = self.phi[i1][j1]
-            m2 = self.marginalize(self.eta, pcopy, i2)
+            m2 = self.marginalize(i2)
             marg2[j1,:] = m2
         rankonefactor = 1
         for i in range(self.k):
@@ -343,7 +400,42 @@ class MOT_Sinkhorn():
                 marg2[j1,j2] += self.rankone[i1][j1] * self.rankone[i2][j2] * rankonefactor
         return marg2
 
-    def save_results(self):
+    def get_pairwise_marginal_old(self, eta, p, rankone, i1, i2):
+        pcopy = copy.deepcopy(p)
+        marg2 = np.zeros((self.ns[i1],self.ns[i2]))
+        # print(np.sum(m1))
+        # print(np.prod([np.sum(rankone[i]) for i in range(self.k)]))
+        # assert(False)
+        for j1 in range(self.ns[i1]):
+            pcopy[i1] = np.ones(self.ns[i1]) * -math.inf
+            pcopy[i1][j1] = p[i1][j1]
+            m2 = self.marginalize(eta, pcopy, i2)
+            marg2[j1,:] = m2
+        rankonefactor = 1
+        for i in range(self.k):
+            if i not in [i1, i2]:
+                rankonefactor *= np.sum(rankone[i])
+        for j1 in range(self.ns[i1]):
+            for j2 in range(self.ns[i2]):
+                marg2[j1,j2] += rankone[i1][j1] * rankone[i2][j2] * rankonefactor
+        return marg2
+
+    def get_pairwise_marginal(self, eta, p, rankone, i1, i2):
+        marg2 = np.zeros((self.ns[i1], self.ns[i2]))
+        pcopy = np.array(p, copy=True)
+        for j1 in range(self.ns[i1]):
+            pcopy[i1, :] = -np.inf
+            pcopy[i1, j1] = p[i1][j1]
+            m2 = self.marginalize(eta, pcopy, i2)
+            marg2[j1, :] = m2
+
+        rankonefactor = np.prod([np.sum(rankone[i]) for i in range(self.k) if i not in [i1, i2]])
+
+        marg2 += np.outer(rankone[i1], rankone[i2]) * rankonefactor
+
+        return marg2
+
+    def save_results(self, X=None):
         tot_loss = np.mean(self.tot_loss[-10:])
         avg_time = np.mean(self.times)
         data_to_save = {
@@ -353,6 +445,7 @@ class MOT_Sinkhorn():
             'times': self.times,
             'params': self.params
             }
+        ot_cost = self.calc_ot_cost()
         # Save path
         path = os.path.join(self.params.figDir, 'results.pkl')
 
@@ -361,7 +454,27 @@ class MOT_Sinkhorn():
             pickle.dump(data_to_save, file)
 
         if self.using_wandb:
-            wandb.log({'tot_loss': tot_loss, 'avg_time': avg_time})
+            wandb.log({'tot_loss': tot_loss,
+                       'avg_time': avg_time,
+                       'ot_cost': ot_cost
+                       })
 
 
-        print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds')
+        print(f'Finished run, loss is {tot_loss:.5f}, average epoch time is {avg_time:.3f} seconds, OT cost is {ot_cost:.5f}')
+
+
+    def calc_emot_from_phi(self):
+        phi = self.phi
+        c = self.C
+        n = phi[0].shape[0]
+        k = len(phi)
+        eps = self.eps
+        e_term = np.eye(n)
+
+        for i in range(k):
+            L = np.exp((0.5 * (phi[i] + phi[(i + 1) % k].T) - c[i]) / eps)
+            e_term = (e_term @ L) * (1 / n)
+
+        exp_term = np.trace(e_term)
+
+        emot = sum(phi).mean() - eps*exp_term + eps

utils/data_fns.py
@@ -3,10 +3,16 @@ import torch
 
 def gen_data(params):
     if params['data_dist'] == 'uniform':
-        # generate k samples which are d-dimensional with n samples (from Taos's notebook)
-        X = []
-        for i in range(params['k']):
-            X.append(np.random.uniform(-1/np.sqrt(params['dims'][i]),1/np.sqrt(params['dims'][i]),(params['n'],params['dims'][i])).astype(np.float32))
+        if params['euler'] == 1:
+            '''
+            generate euler flow samples - n evenly spaces samples along [0,1]
+            '''
+            X = [np.linspace(0, 1, params['n'], dtype=np.float32).reshape(params['n'], 1).astype(np.float32)]*params['k']
+        else:
+            # generate k samples which are d-dimensional with n samples (from Taos's notebook)
+            X = []
+            for i in range(params['k']):
+                X.append(np.random.uniform(-1/np.sqrt(params['dims'][i]),1/np.sqrt(params['dims'][i]),(params['n'],params['dims'][i])).astype(np.float32))
 
 
         if params['alg'] not in ('ne_gw','ne_mot'):
@@ -124,6 +130,32 @@ def QuadCost(data, mod='circle'):
 
                     # Sum the broadcasted norms into the tensor
                     differences += norms_reshaped
+    elif mod == 'euler':
+        '''
+        Calculate Euler Flows cost graph.
+        The Euler cost is defined as:
+        c(x_1...x_k) = \|\sigma(x_1) - x_k\|^2 + \sum_{i=1}^k\|x_{i+1}-x_i\|^2
+        '''
+        differences = []
+        if isinstance(data, np.ndarray):
+            pass
+        else:
+            for i in range(k-1):
+                vectors_i = data[:, :, i].unsqueeze(1)
+                vectors_j = data[:, :, (i + 1) % k].unsqueeze(0)
+
+                # Compute the norm of the vector differences
+                vector_diffs = vectors_i - vectors_j
+                norms = torch.norm(vector_diffs, dim=2) ** 2  # Compute norms along the vector dimension
+                differences.append(norms)
+
+            vectors_i = data[:, :, k-1].unsqueeze(1)
+            vectors_j = EulerSigma(data[:, :, 0].unsqueeze(0))
+            vector_diffs = vectors_i - vectors_j
+            norms = torch.norm(vector_diffs, dim=2) ** 2  # Compute norms along the vector dimension
+            differences.append(norms)
+
+
 
     return differences
 
@@ -138,3 +170,15 @@ def kronecker_product(vectors):
 
 def calc_ent(p):
     return -np.sum(p*np.log(p))
+
+
+def EulerSigma(data, case=0):
+    """
+    Applies the Euler Flow sigma displacement function.
+    :param data: assumed to be one-dimensional, so shape is (n,d,1)
+    :return:
+    """
+    if case == 0:
+        # in this case its x = (x + 0.5) mod 1
+        data = (data + 0.5)%1
+        return data